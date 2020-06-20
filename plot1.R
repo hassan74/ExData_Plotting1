973f5f7d2dbd5e22f38e116064e7d8822ab992e3
Zain <ahmedzain958@gmail.com>
2020-06-20 13:04:58 +0200

    plot1 change

--- plot1.R
+++ plot1.R
@@ -8,6 +8,6 @@ df$date <-  as.Date(df$date, format="%d/%m/%Y")
 df <- df[df$date >= as.Date("2007-02-01") & df$date<=as.Date("2007-02-02"),]
 
 
-png(filename="plot1.png", width=480, height=480, units="px")
+png(filename="ExData_Plotting1\plot1.png", width=480, height=480, units="px")
 hist(df$globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
